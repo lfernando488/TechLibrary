cc5893ec3b6ce2e874fe45e4550c051784118709
Luiz Fernando <lfernando488@gmail.com>
2025-02-18 22:21:05 -0300

    Adicionar arquivos de projeto.

---
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechLibrary.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return Created();
+        }
+
+    }
+}
